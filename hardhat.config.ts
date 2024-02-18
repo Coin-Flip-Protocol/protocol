c68a9b669f9c707032b2ca4afa5e2aaa05a2f606
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2024-02-18 20:40:21 +0400

    feat: deploy contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    blast_sepolia: {
+      chainId: 168587773,
+      url: "https://sepolia.blast.io",
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      blast_sepolia: "blast_sepolia", // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: "blast_sepolia",
+        chainId: 168587773,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan",
+          browserURL: "https://testnet.blastscan.io",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
