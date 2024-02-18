605992b22d93696a9d3f4d79a3c2f4a928b88128
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2024-02-18 19:57:36 +0400

    wip: added blast yield

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./IBlast.sol";
+
 contract CoinFlip {
+    IBlast immutable yieldContract =
+        IBlast(0x4300000000000000000000000000000000000002);
+
     // Event showing results of the Flip Game
     event FlipResult(
         address indexed player,
@@ -24,6 +29,13 @@ contract CoinFlip {
     uint public totalHeadsBets;
     uint public totalTailsBets;
 
+    constructor() {
+        // Initialise Blast Eth Yield if it's Blast Chain
+        if (block.chainid == 168587773) {
+            yieldContract.configureClaimableYield();
+        }
+    }
+
     // Function for players to place bets
     function placeBet(bool isHeads) external payable {
         require(msg.value > 0, "Bet amount must be greater than 0");
@@ -41,6 +53,9 @@ contract CoinFlip {
     // Function to execute the coin flip
     function flipCoin() external {
         require(addresses.length > 0, "No bets placed");
+
+        // TODO: Implement Chainlink VRF on Mainnet: https://docs.chain.link/vrf
+        // We can't rely on timestap for random result generation
         bool flipResult = (block.timestamp % 2 == 0);
 
         // Iterate through all players and determine winners
@@ -96,5 +111,11 @@ contract CoinFlip {
             address betAddress = addresses[i];
             delete bets[betAddress];
         }
+    }
+
+    // Claim Eth Yield
+    function claimAllYield(address recipient) external {
+        //This function is public meaning anyone can claim the yield
+        yieldContract.claimAllYield(address(this), recipient);
     }
 }
