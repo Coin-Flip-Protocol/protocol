ebb7550e9ffc98062e77bb9576f7ebc34c924884
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2024-02-21 00:06:41 +0400

    fix: layout

--- frontend/src/components/Connect.tsx
+++ frontend/src/components/Connect.tsx
@@ -3,16 +3,19 @@ import BtnConnect from "./BtnConnect";
 
 export default function Connect() {
   return (
-    <>
+    <div className="w-[1000px]">
       <h2 className="text-[#212121] font-bold md:text-[72px] text-[45px]">
         Flip a coin & Win!
       </h2>
-      <p className="font-normal text-[20px] w-[] text-[#424242] pt-[20px] max-w-[99%]">
+      <p className="font-normal text-[20px] w-[600px] mx-auto text-[#424242] pt-[20px] max-w-[99%]">
         To play, users predict "heads" or "tails" in a coin toss. If their guess
         matches the outcome, they win. Gamble responsibly, setting limits on
         spending and time.
       </p>
-      <BtnConnect />
-    </>
+
+      <div className="flex align-middle justify-center mt-8">
+        <BtnConnect />
+      </div>
+    </div>
   );
 }
